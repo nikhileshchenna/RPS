5d6287db6f81603e7c11ec60c1fcb71a2ab4981f
nikhileshchenna <chennanikhilesh@gmail.com>
2026-03-01 15:08:14 +0530

    again commented

--- loader.js
+++ loader.js
@@ -1,8 +1,8 @@
-const sub = document.getElementById("sub");
-sub.onclick = (event) => {
-  event.preventDefault();
+// const sub = document.getElementById("sub");
+// sub.onclick = (event) => {
+//   event.preventDefault();
 
-  setTimeout(() => {
-    window.location.href = "rock.html";
-  }, 3000);
-};
+//   setTimeout(() => {
+//     window.location.href = "rock.html";
+//   }, 3000);
+// };
